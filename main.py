6cb3e9c72094f5396c36ef4e64a34295d6c1a733
Einar Pálsson <epp5@hi.is>
2022-12-15 18:53:13 +0000

    updated req.txt

--- main.py
+++ main.py
@@ -9,6 +9,18 @@ from Models.Base import Base
 from Models.History import History
 from Models.Library import Library
 
+'''
+-- Libraries (AllTime) --
+R   GET /library/{id}
+L   GET /libraries --PARAMS
+
+-- History --
+R   GET /library/history/{lib_id}
+U   UPDATE /library/history/{lib_id}
+D   DELETE /library/history/{lib_id}
+L   GET /libraries/history --PARAMS
+'''
+
 app = FastAPI()
 
 engine = create_engine("postgresql+psycopg2://localhost:5432/smileylibs?user=postgres&password=12345")
